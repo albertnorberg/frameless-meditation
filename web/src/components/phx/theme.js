15580da50dfa5688ca771d05f6e9fa7a8c731fce
Albert Norberg <alb.norberg@gmail.com>
2020-12-20 19:18:47 +0100

    - Add font Questrial - Change them color to FM

--- web/src/components/phx/theme.js
+++ web/src/components/phx/theme.js
@@ -1,15 +1,17 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
-import green from '@material-ui/core/colors/green';
 
 export const theme = createMuiTheme({
   palette: {
     type: 'dark',
     primary: {
-      main: blue[500],
+      light: '#6eddcd',
+      main: '#36AB9C',
+      dark: '#007b6e',
+      contrastText: '#fff',
     },
     secondary: {
-      main: green[500],
+      main: blue[500],
     },
   },
 });
